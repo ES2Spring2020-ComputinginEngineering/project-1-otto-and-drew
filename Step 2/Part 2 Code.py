16016d22bc53c0dc89d189b25b2fe63c17e5fea6
drewhollett <60231007+drewhollett@users.noreply.github.com>
2020-02-29 10:34:40 -0500

    Create Part 2 Code.py

---
+++ Step 2/Part 2 Code.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 10:10:26 2020
+
+@author: drewh
+"""
+
+import numpy as np
+#import math as mt
+
+length = [.140, .191, .241, .292, .343]
+arr1d = np.array(length)
+
+def period(a):
+    t = (a/9.8)**.5
+    return t
