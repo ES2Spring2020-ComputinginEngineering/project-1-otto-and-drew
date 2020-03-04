1f4a15743812451d8475c089846facb2dd7a59a9
ottolaakso <otto.laakso910@gmail.com>
2020-03-04 14:22:16 -0500

    1.0

--- Step 4/analysis.py
+++ Step 4/analysis.py
@@ -1,6 +1,6 @@
 # Step 4: Analysis of Results
 
-import os
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,11 +12,27 @@ fin5 = open("receiver_data5.txt", "r")
 
 def create_array(fin):
     array = []
-    for line in fin:
-        row = line.split()
+    for row in fin:
         array = np.append(array, row)
     return array
 
 def new_array(array):
-    new_array = []
-    array.index()
+    
+    new_list = []
+    time_value = []
+    acc_values = []
+   
+    for row in array:
+        time_value = array[np.array(0)]
+        acc_values = array[np.array(1,4)]
+        acc_x = acc_values[0]
+        acc_y = acc_values[1]
+        acc_z = acc_values[2]
+        
+        tilt_x = 
+        theta = x
+        new_list = new_list + time_value + theta
+    
+    new_array = np.array(new_list)
+    return new_array
+   
