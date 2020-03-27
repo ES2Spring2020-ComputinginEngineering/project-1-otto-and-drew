841b387389622f3b2f5f2bed0749835c842e3cb0
ottolaakso <otto.laakso910@gmail.com>
2020-03-27 14:51:18 +0200

    1.0

--- Step 5/numerical model.py
+++ Step 5/numerical model.py
@@ -1,7 +1,35 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 21 10:32:17 2020
+import os
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: drewh
-"""
+def update_system(pos,vel, acc):
+    
+    dt = 0.1
+    pos_next = 
+    vel_next = 
+    acc_next = 
+    
+    return pos_next, vel_next, acc_next
 
+pos = [60]
+vel = [0]
+acc = [0]
+time = np.arange(0, 20, 0.1)
+values = []
+i = 1
+    while i < len(time):
+    
+        pos_next, vel_next, acc_next = update_system(pos[i], vel[i], acc[i])
+        time_next = time(i)
+    
+        pos.append(pos_next)
+        vel.append(vel_next)
+        acc.append(acc_next)
+        values.append(time_next, pos_next, vel_next, acc_next)
+
+    array = np.array(values)
+
+    
+    
