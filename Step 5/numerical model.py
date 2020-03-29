5b973e3e5f99f4c39aac09d54c808742e60ad1c2
drewhollett <60231007+drewhollett@users.noreply.github.com>
2020-03-29 14:58:55 -0400

    Add files via upload

--- Step 5/numerical model.py
+++ Step 5/numerical model.py
@@ -1,77 +1,96 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.signal as sig
 import math
+import scipy.signal as sig
 
 g = 9.81
 L = [.14, .19, .24, .29, .34]
-pos = [0]
-vel = [0]
-acc = [0,1,2,3,4,4,2,2,1,0,0,0,0,-1,-1,-2,-2,-2,-3,-4,-4]
-time = np.linspace(0,20,21)
-theta = np.radians(110)
 
-def update_system(acc,pos,vel,time1,time2):
+
+
+def update_system(pos,vel,time1,time2, length):
     # position and velocity update below
     dt = time2-time1
-    posNext = pos+vel*dt
-    velNext = vel+(g/L[4])*np.sin(theta)*dt
-    return posNext,velNext
+    accNext = (g/length)*np.sin(pos)
+    velNext = vel+(accNext*dt)
+    posNext = pos+(velNext*dt)
+    return posNext,velNext, accNext
 
-def find_period(length):
-    s = (length/g)
-    y = math.sqrt(s)
-    x = 2 * math.pi
-    print(y * x)
-    
-   
-i = 1
-while i < len(time):
+
+
+def pen_sim(length, start_angle):
+    if start_angle < 90:
+        pos = [math.pi/3]
+    elif start_angle > 90:
+        pos = [2*math.pi/3]
+    vel = [0]
+    acc = [0]
+    time = np.arange(0,20,.1)
+    i = 1
+    while i < len(time):
     # update position and velocity using previous values and time step
-    posNext, velNext = update_system(acc[i],pos[i-1],vel[i-1],time[i-1],time[i])
-    pos.append(posNext)
-    vel.append(velNext)
-    #print_system(time[i],pos[i],vel[i])
-    i += 1
+        posNext, velNext, accNext = update_system(pos[i-1],vel[i-1],time[i-1],time[i], length)
+        pos.append(posNext)
+        vel.append(velNext)
+        acc.append(accNext)
+        i += 1
 
 
-plt.subplot(3,1,1)
-plt.plot(time, pos, 'ro--') 
-plt.xlabel('Time (seconds)')
-plt.ylabel('Position (m)')
-plt.title('Position vs Time .34 meters at 110 deg')
-plt.xlim((0, 20)) # set x range to -1 to 8
-plt.grid()
+    plt.subplot(3,1,1)
+    plt.plot(time, pos, 'r--') 
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Position (m)')
+    plt.title('Position vs Time at ' + str(length) + ' meters and ' + str(start_angle) + ' deg')
+    plt.xlim((0, 20)) # set x range to -1 to 8
+    plt.grid()
 
 
-plt.subplot(3,1,2)
-plt.plot(time, vel, 'ro--') 
-plt.xlabel('Time (seconds)')
-plt.ylabel('Velocity (m/s)')
-plt.title('Velocity vs Time')
-plt.xlim((0, 20)) # set x range to -1 to 8
-plt.grid()
+    plt.subplot(3,1,2)
+    plt.plot(time, vel, 'r--') 
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Velocity (m/s)')
+    plt.title('Velocity vs Time')
+    plt.xlim((0, 20)) # set x range to -1 to 8
+    plt.grid()
 
 
-plt.subplot(3,1,3)
-plt.plot(time, acc, 'ro--') 
-plt.xlabel('Time (seconds)')
-plt.ylabel('Acceleration (m/s^2)')
-plt.title('Acceleration vs Time')
-plt.xlim((0, 20)) # set x range to -1 to 8
-plt.grid()
-plt.tight_layout()
-plt.show()
+    plt.subplot(3,1,3)
+    plt.plot(time, acc, 'r--') 
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Acceleration (m/s^2)')
+    plt.title('Acceleration vs Time')
+    plt.xlim((0, 20)) # set x range to -1 to 8
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
     
-p = [.24, .28, .31, .34, .37]
-#these values were obtained by running the function find_period
+    p_filt_pks, _ = sig.find_peaks(pos)
+    pt = time[p_filt_pks]
+    period = pt[5] - pt[4]
+    print('Period of pendulum ' + str(length) + ' meters and the period is ' + str(period) + 'sec')
+   
+pen_sim(.14, 60)    
+pen_sim(.19, 60) 
+pen_sim(.24, 60)
+pen_sim(.29, 60) 
+pen_sim(.34, 60) 
+pen_sim(.14, 120) 
+pen_sim(.19, 120) 
+pen_sim(.24, 120) 
+pen_sim(.29, 120) 
+pen_sim(.34, 120) 
+
+
+
+p = [1.0, 1.2,  1.299, 1.5, 1.6]
+#these values were obtained by running the function pen_sim
 
 plt.plot(L, p, 'bo-')
 plt.xlabel('Length (m)')
 plt.ylabel('Period (s)')
 plt.grid(True)
-plt.title('Numerical Correlation of Pendulum Length vs. Period')
+plt.title('Numerical Correlation of Pendulum Length vs. Period at 60 deg')
 plt.show()
 
 
