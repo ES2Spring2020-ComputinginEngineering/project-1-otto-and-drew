e85e86d2a281ed5d1b9fe175a0adfdec34d98d4e
drewhollett <60231007+drewhollett@users.noreply.github.com>
2020-03-30 12:42:44 -0400

    Add files via upload

--- Step 5/numerical model.py
+++ Step 5/numerical model.py
@@ -1,5 +1,4 @@
-""" Step 5 Numerical Model Code """
-
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -8,15 +7,17 @@ import scipy.signal as sig
 g = 9.81
 L = [.14, .19, .24, .29, .34]
 
-def update_system(pos,vel,time1,time2, length):
-# Takes position, velocity, time1, time2, and length values as parameters
-# Calculates and returns next values for  position, velocity, and acceleration
 
+
+def update_system(pos,vel,time1,time2, length):
+    # position and velocity update below
     dt = time2-time1
     accNext = (g/length)*np.sin(pos)
     velNext = vel+(accNext*dt)
     posNext = pos+(velNext*dt)
     return posNext,velNext, accNext
+
+
 
 def pen_sim(length, start_angle):
     if start_angle < 90:
@@ -35,6 +36,7 @@ def pen_sim(length, start_angle):
         acc.append(accNext)
         i += 1
 
+
     plt.subplot(3,1,1)
     plt.plot(time, pos, 'r--') 
     plt.xlabel('Time (seconds)')
@@ -43,6 +45,7 @@ def pen_sim(length, start_angle):
     plt.xlim((0, 20)) # set x range to -1 to 8
     plt.grid()
 
+
     plt.subplot(3,1,2)
     plt.plot(time, vel, 'r--') 
     plt.xlabel('Time (seconds)')
@@ -50,6 +53,7 @@ def pen_sim(length, start_angle):
     plt.title('Velocity vs Time')
     plt.xlim((0, 20)) # set x range to -1 to 8
     plt.grid()
+
 
     plt.subplot(3,1,3)
     plt.plot(time, acc, 'r--') 
@@ -77,6 +81,8 @@ pen_sim(.24, 120)
 pen_sim(.29, 120) 
 pen_sim(.34, 120) 
 
+
+
 p = [1.0, 1.2,  1.299, 1.5, 1.6]
 #these values were obtained by running the function pen_sim
 
@@ -86,3 +92,25 @@ plt.ylabel('Period (s)')
 plt.grid(True)
 plt.title('Numerical Correlation of Pendulum Length vs. Period at 60 deg')
 plt.show()
+
+p1 = [.799, 1, 1, 1.2, 1.2]
+
+plt.plot(L, p1, 'bo-')
+plt.xlabel('Length (m)')
+plt.ylabel('Period (s)')
+plt.grid(True)
+plt.title('Numerical Correlation of Pendulum Length vs. Period at 120 deg')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
