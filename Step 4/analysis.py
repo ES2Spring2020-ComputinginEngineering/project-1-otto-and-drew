42571a3f7e3efa2dd757e721e44a2b865222e3cc
drewhollett <60231007+drewhollett@users.noreply.github.com>
2020-03-31 13:16:10 -0400

    Add files via upload

--- Step 4/analysis.py
+++ Step 4/analysis.py
@@ -1,5 +1,4 @@
-""" Step 4 Real-World Model Code """
-""" Otto Laakso & Drew Hollett """
+# Step 4: Analysis of Results
 
 import os
 import math
@@ -7,8 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 
-path = "/Users/ottolaakso/Desktop/Tufts Classes/ES2/GitHub/project-1-otto-and-drew/Step 3"
-#path = "c:/Users/drewh/Documents/GitHub/project-1-otto-and-drew/Step 3"
+#path = "/Users/ottolaakso/Desktop/Tufts Classes/ES2/GitHub/project-1-otto-and-drew/Step 3"
+path = "c:/Users/drewh/Documents/GitHub/project-1-otto-and-drew/Step 3"
 os.chdir(path)
 
 fin1 = open("receiver_data1.txt", "r")
@@ -18,11 +17,13 @@ fin4 = open("receiver_data4.txt", "r")
 fin5 = open("receiver_data5.txt", "r")
 #These variables represent the data files for each of the five pendulum lengths
 
+L = [.14, .19, .24, .29, .34]
+#These are the pendulum length in meters
+
 def create_array(name):
 # Takes the name of a data file as a parameter and turns the contents of 
 # the file into an array
-# Returns the array
-    
+# Returns the array    
     fin = open(name, "r")
     outname = "new" + name
     fout = open(outname, "w")
@@ -36,27 +37,28 @@ def create_array(name):
     array = np.loadtxt(outname, delimiter=",", dtype=float)
     return array
 
-def acc_array(array):
-# Takes an array of time and x, y, and z acceleration values
+
+def acc_array(array, length):
+# Takes an array of time and x, y, and z acceleration values and length as 2 parameters
 # Graphs the x, y, and z acceleration values in relation to time
-# Void function
-    
+# Void function    
     t = array[:,0]
     x = array[:,1]
     y = array[:,2]
     z = array[:,3]
     plt.plot(t, x, 'r--', t, y, 'bs', t, z, 'g^')
-    plt.xlabel('Time')
-    plt.ylabel('Accelerations')
-    plt.title('Acceleration vs Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Accelerations (m/s^2)')
+    plt.title('Acceleration vs Time ' + str(length) + ' meters')
     plt.legend(('X values', 'Y values', 'Z values'))
     plt.show()
+
 
 def theta_array(array):
 # Takes an array of time and x, y, and z acceleration values
 # creates a new array containing the angle of the pendulum at each time value.
-# Returns the new array with theta and time.
-     
+# Returns the new array with theta and time.     
+    
     new_list = []
     time = array[0:,0]
     acc_x = array[:,1]
@@ -72,20 +74,20 @@ def theta_array(array):
     new_array = np.array(new_list)
     return new_array
 
-def theta_graph(new_array):
-# Takes an array with angle and time values as a parameter and graphs them
+def theta_graph(new_array, length):
+# Takes an array with angle and time values as a parameter and length as another parameter and graphs them
 # Void function
     
     ti = new_array[:,0]
     th = new_array[:,1]
     plt.plot(ti, th, 'b.')
-    plt.xlabel('Time')
-    plt.ylabel('Theta')
-    plt.title('Theta vs. Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Theta in degrees')
+    plt.title('Theta vs. Time ' + str(length) + ' meters')
     plt.show()
 
-def find_period(new_array):
- # Takes an array of time and pendulum angle values as a parameter
+def find_period(new_array, length):
+ # Takes an array of time and pendulum angle values as a parameter as well as length
  # Calculates the period and graphs period vs. time
  # Void function
  
@@ -94,30 +96,72 @@ def find_period(new_array):
     y_filt = sig.medfilt(y)
     y_filt_pks, _ = sig.find_peaks(y_filt)
     plt.plot(time, y, 'r-', time[y_filt_pks], y_filt[y_filt_pks], 'b.')
-    plt.title('Period of pendulum 13.5 inches')
+    plt.title('Period of pendulum ' + str(length) + ' meters')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Theta in degrees')
     plt.show()
-    print(y_filt[y_filt_pks])
-
+    
 s = create_array("receiver_data1.txt")
-p = create_array("receiver_data2.txt")
+v = create_array("receiver_data2.txt")
 q = create_array("receiver_data3.txt")
 r = create_array("receiver_data4.txt")
 t = create_array("receiver_data5.txt")
 #These variables will simplify the plotting process
 
 l = theta_array(s)
-m = theta_array(p)
+m = theta_array(v)
 n = theta_array(q)
 o = theta_array(r)
 p = theta_array(t)
-#these variables will make it easier ti create the graph and find the period
+#these variables will make it easier to create the graph and find the period
 
-p = [.64, .67, .71, .73, .75]
-L = [0.14, 0.191, 0.241, 0.292, 0.343]
-# Finally, our code creates a graph that compares the period against the five lengths of the pendulum
-plt.plot(L, p, "bo-")
-plt.xlabel('Length (m)')
-plt.ylabel('Period (s)')
-plt.title('Real-World Correlation of Pendulum Length vs. Period')
-plt.grid(True)
-plt.show()
+def main():
+#Main will create all the graphs and period comparison
+#Takes no parameters
+#Void function
+    
+    acc_array(s, .14)
+    acc_array(v, .19)
+    acc_array(q, .24)
+    acc_array(r, .29)
+    acc_array(t, .34)
+    
+    theta_graph(l, .14)
+    theta_graph(m, .19)
+    theta_graph(n, .24)
+    theta_graph(o, .29)
+    theta_graph(p, .34)
+    
+    find_period(l, .14)
+    find_period(m, .19)
+    find_period(n, .24)
+    find_period(o, .29)
+    find_period(p, .34)
+    
+    pr = [.64, .67, .71, .73, .75]
+
+    plt.plot(L, pr, 'r--', L, pr, 'b.')
+    plt.xlabel('Length in m')
+    plt.ylabel('Period in s')
+    plt.title('Length vs Period Real-World')
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
