6eea82d1f212acf2ec0431918a7191561d877dcc
drewhollett <60231007+drewhollett@users.noreply.github.com>
2020-03-31 13:22:04 -0400

    Add files via upload

--- Step 5/numerical model.py
+++ Step 5/numerical model.py
@@ -1,22 +1,26 @@
-""" Step 5 Numerical Model Code """
-""" Otto Laakso & Drew Hollett """
-
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 import scipy.signal as sig
 
 g = 9.81
+#gravity constant
+
 L = [.14, .19, .24, .29, .34]
+#The lengths for our pendulum
+
 
 def update_system(pos,vel,time1,time2, length):
 # Takes position, velocity, time1, time2, and length as parameters
-# Calculates and returns next acceleration, velocity, and position values
+# Calculates and returns next acceleration, velocity, and position values 
     dt = time2-time1
     accNext = (g/length)*np.sin(pos)
     velNext = vel+(accNext*dt)
     posNext = pos+(velNext*dt)
     return posNext,velNext, accNext
+
+
 
 def pen_sim(length, start_angle):
 # Takes the length and starting angle of the pendulum as parameters
@@ -37,6 +41,8 @@ def pen_sim(length, start_angle):
         vel.append(velNext)
         acc.append(accNext)
         i += 1
+
+
     plt.subplot(3,1,1)
     plt.plot(time, pos, 'r--') 
     plt.xlabel('Time (seconds)')
@@ -45,6 +51,7 @@ def pen_sim(length, start_angle):
     plt.xlim((0, 20)) # set x range to -1 to 8
     plt.grid()
 
+
     plt.subplot(3,1,2)
     plt.plot(time, vel, 'r--') 
     plt.xlabel('Time (seconds)')
@@ -52,6 +59,7 @@ def pen_sim(length, start_angle):
     plt.title('Velocity vs Time')
     plt.xlim((0, 20)) # set x range to -1 to 8
     plt.grid()
+
 
     plt.subplot(3,1,3)
     plt.plot(time, acc, 'r--') 
@@ -62,41 +70,58 @@ def pen_sim(length, start_angle):
     plt.grid()
     plt.tight_layout()
     plt.show()
-
+    
     p_filt_pks, _ = sig.find_peaks(pos)
     pt = time[p_filt_pks]
     period = pt[5] - pt[4]
     print('Period of pendulum ' + str(length) + ' meters and the period is ' + str(period) + 'sec')
    
-pen_sim(.14, 60)    
-pen_sim(.19, 60) 
-pen_sim(.24, 60)
-pen_sim(.29, 60) 
-pen_sim(.34, 60) 
-pen_sim(.14, 120) 
-pen_sim(.19, 120) 
-pen_sim(.24, 120) 
-pen_sim(.29, 120) 
-pen_sim(.34, 120) 
+def main():
+#Will take previous functions and create readable graphs for them
+#Takes no parameters
+#Void function
+    
+    
+    pen_sim(.14, 60)    
+    pen_sim(.19, 60) 
+    pen_sim(.24, 60)
+    pen_sim(.29, 60) 
+    pen_sim(.34, 60) 
+    pen_sim(.14, 120) 
+    pen_sim(.19, 120) 
+    pen_sim(.24, 120) 
+    pen_sim(.29, 120) 
+    pen_sim(.34, 120) 
 
-p = [1.0, 1.2,  1.299, 1.5, 1.6]
-# Period values for pendulum starting at 60 degrees
-# Obtained from running pen_sim for five lengths at 60 degrees
+    p = [1.0, 1.2, 1.299, 1.5, 1.6]
+    #these values were obtained by running the function pen_sim with 60 degrees
 
-plt.plot(L, p, 'bo-')
-plt.xlabel('Length (m)')
-plt.ylabel('Period (s)')
-plt.grid(True)
-plt.title('Numerical Correlation of Pendulum Length vs. Period at 60 deg')
-plt.show()
+    plt.plot(L, p, 'bo-')
+    plt.xlabel('Length (m)')
+    plt.ylabel('Period (s)')
+    plt.grid(True)
+    plt.title('Numerical Correlation of Pendulum Length vs. Period at 60 deg')
+    plt.show()
 
-p1 = [.799, 1, 1, 1.2, 1.2]
-# Period values for pendulum starting at 120 degrees
-# Obtained from running pen_sim for five lengths at 120 degrees
+    p1 = [.799, 1, 1, 1.2, 1.2]
+    #These values were obtained by running the function pen_sim with 120 degrees
 
-plt.plot(L, p1, 'bo-')
-plt.xlabel('Length (m)')
-plt.ylabel('Period (s)')
-plt.grid(True)
-plt.title('Numerical Correlation of Pendulum Length vs. Period at 120 deg')
-plt.show()
+    plt.plot(L, p1, 'bo-')
+    plt.xlabel('Length (m)')
+    plt.ylabel('Period (s)')
+    plt.grid(True)
+    plt.title('Numerical Correlation of Pendulum Length vs. Period at 120 deg')
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
